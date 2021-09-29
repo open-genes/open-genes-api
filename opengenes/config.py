0b2da2a5afcb0e040590841bde475f0a0a66c938
Sergey Pushkin <noc@acs.com>
2021-09-29 16:58:48 +0700

    config & openapi url fixes for production environment

--- opengenes/config.py
+++ opengenes/config.py
@@ -2,10 +2,10 @@ import os
 import sys
 from enum import Enum
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
-CONFIG = dotenv_values(os.path.join(sys.path[1], '.env'))
-
+load_dotenv(os.path.join(sys.path[1], '.env'))
+CONFIG = os.environ
 
 class Language(Enum):
     ru = "ru"
