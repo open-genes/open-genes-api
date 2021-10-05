7443f012901a0741dadca9818ba2f9b19f1ab74f
Sergey Pushkin <noc@acs.com>
2021-10-06 02:53:25 +0700

    verbose build with version in the log

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 
-echo build=`git log HEAD --pretty=oneline --relative .| wc -l` "\n"revision=\"`git rev-parse --short HEAD`\""\n"date=`git rev-list --date=short --pretty=%cd HEAD |sed -n 2p` >VERSION
+BUILD=`git log HEAD --pretty=oneline --relative .| wc -l`
+REVISION=`git rev-parse --short HEAD`
+DATE=`git rev-list --date=short --pretty=%cd HEAD |sed -n 2p`
+echo "build=\"$BUILD\"\nrevision=\"$REVISION\"\ndate=\"$DATE\"" >VERSION
+echo VERSION file written with build $BUILD revision $REVISION date $DATE
 
 [ -f .env ] || cat /dev/null >.env
 docker build -t opengenes/api .
