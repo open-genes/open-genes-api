5fe0d035666a3a52f1408ccb28380bf526a8f1ba
yura <ledovskij.yura@gmail.com>
2021-10-14 13:53:15 +0700

    OG-210 Some fixes, adds parameters

--- opengenes/db/filters.py
+++ opengenes/db/filters.py
@@ -1,6 +1,6 @@
 FILTERS = {
     'diseases': ' `disease`.`id` IN ({}) ',
-    'disease_categories': ' `parent_icd_code`.`id` IN ({}) ',
-    'functional_clusters': ' `functionalClusters`.`id` IN ({}) ',
+    'disease_categories': ' `disease`.`parent_icd_code` IN ({}) ',
+    'functional_clusters': ' `functional_cluster`.`id` IN ({}) ',
     'expression_change': ' `gene`.`expressionChange` IN ({}) ',
 }
--- opengenes/db/sql_raws/scripts.py
+++ opengenes/db/sql_raws/scripts.py
@@ -1,39 +1,39 @@
 GENES_QUERY = '''
 SELECT JSON_OBJECT(
-'options',JSON_OBJECT('pagination',JSON_OBJECT('page',@PAGE@,'pageSize',@PAGESIZE@,'pageTotal',CEILING(jsout.fRows/@PAGESIZE@)),'objTotal',jsout.fRows)
+'options',JSON_OBJECT('pagination',JSON_OBJECT('page',@PAGE@,'pageSize',@PAGESIZE@,'pageTotal',CEILING(MAX(jsout.fRows)/@PAGESIZE@)),'objTotal',MAX(jsout.fRows))
 ,'items',JSON_ARRAYAGG(jsout.jsonobj)) respJS FROM (
 SELECT preout.jsonobj, fRows FROM (
 SELECT count(*) OVER() fRows,
 JSON_OBJECT(
-'id',`gene`.`id`,
-'homologueTaxon',IFNULL(`taxon`.`name_en`,''),
-'symbol',IFNULL(`gene`.`symbol`,''),
-'name',IFNULL(`gene`.`name`,''),
-'ncbiId',`gene`.`ncbi_id`,
-'uniprot',IFNULL(`gene`.`uniprot`,''),
-'expressionChange',`gene`.`expressionChange`,
-'timestamp',`gene`.`updated_at`,
-'ensembl',`gene`.`ensembl`,
-'methylationCorrelation',IFNULL(`gene`.`methylation_horvath`,''),
-'aliases',CAST(CONCAT_WS('"','[',REPLACE(`gene`.`aliases`,' ','","'),']') AS JSON),
+'id',gene.id,
+'homologueTaxon',IFNULL(taxon.name_en,''),
+'symbol',IFNULL(gene.symbol,''),
+'name',IFNULL(gene.name,''),
+'ncbiId',gene.ncbi_id,
+'uniprot',IFNULL(gene.uniprot,''),
+'expressionChange',gene.expressionChange,
+'timestamp',gene.updated_at,
+'ensembl',gene.ensembl,
+'methylationCorrelation',IFNULL(gene.methylation_horvath,''),
+'aliases',CAST(CONCAT_WS('"','[',REPLACE(gene.aliases,' ','","'),']') AS JSON),
 'origin',JSON_OBJECT(
- 'id',`phylum`.`id`,
- 'phylum',IFNULL(`phylum`.`name_phylo`,''),
- 'age',IFNULL(`phylum`.`name_mya`,''),
- 'order',`phylum`.`order`
+ 'id',phylum.id,
+ 'phylum',IFNULL(phylum.name_phylo,''),
+ 'age',IFNULL(phylum.name_mya,''),
+ 'order',phylum.order
  ),
 'familyOrigin',JSON_OBJECT(
- 'id',`family_phylum`.`id`,
- 'phylum',IFNULL(`family_phylum`.`name_phylo`,''),
- 'age',IFNULL(`family_phylum`.`name_mya`,''),
- 'order',`family_phylum`.`order`
+ 'id',family_phylum.id,
+ 'phylum',IFNULL(family_phylum.name_phylo,''),
+ 'age',IFNULL(family_phylum.name_mya,''),
+ 'order',family_phylum.order
  ),
-'commentCause',JSON_REMOVE(JSON_OBJECTAGG(IFNULL(comment_cause.id,'null__'),comment_cause.name_en), '$.null__'),
+'commentCause',JSON_REMOVE(JSON_OBJECTAGG(IFNULL(comment_cause.id,'null'),comment_cause.name_en), '$.null'),
 'functionalClusters',CAST(CONCAT('[',GROUP_CONCAT(distinct JSON_OBJECT('id',IFNULL(functional_cluster.id,0),'name',IFNULL(functional_cluster.name_en,'')) separator ","),']') AS JSON), ## этот вариант требует больше памяти, но обеспечивает совместимость со старым фронтом
-##'functionalClusters',JSON_REMOVE(JSON_ARRAYAGG(JSON_OBJECT('id',IFNULL(functional_cluster.id,'null__'),'name',IFNULL(functional_cluster.name_en,''))), '$.null__'), ## этот вариант требует больше памяти, но обеспечивает совместимость со старым фронтом
+##'functionalClusters',JSON_REMOVE(JSON_ARRAYAGG(JSON_OBJECT('id',IFNULL(functional_cluster.id,'null'),'name',IFNULL(functional_cluster.name_en,''))), '$.null'), ## этот вариант требует больше памяти, но обеспечивает совместимость со старым фронтом
 ##'functionalClusters',JSON_OBJECTAGG('id',IFNULL(functional_cluster.id,''),'name',IFNULL(functional_cluster.name_en,'')), ## этот вариант требует больше памяти, но обеспечивает совместимость со старым фронтом
-'diseases',JSON_REMOVE(JSON_OBJECTAGG(IFNULL(disease.id,'null__'),JSON_OBJECT('icdCode',IFNULL(disease.icd_code ,''),'name',IFNULL(disease.name_en,''),'icdName',IFNULL(disease.icd_name_en,''))), '$.null__'),
-'diseaseCategories',JSON_REMOVE(JSON_OBJECTAGG(IFNULL(disease_category.id,'null__'),JSON_OBJECT('icdCode',IFNULL(disease_category.icd_code,''),'icdCategoryName',IFNULL(disease_category.icd_name_en,''))), '$.null__')
+'diseases',JSON_REMOVE(JSON_OBJECTAGG(IFNULL(disease.id,'null'),JSON_OBJECT('icdCode',IFNULL(disease.icd_code ,''),'name',IFNULL(disease.name_en,''),'icdName',IFNULL(disease.icd_name_en,''))), '$.null'),
+'diseaseCategories',JSON_REMOVE(JSON_OBJECTAGG(IFNULL(disease_category.id,'null'),JSON_OBJECT('icdCode',IFNULL(disease_category.icd_code,''),'icdCategoryName',IFNULL(disease_category.icd_name_en,''))), '$.null')
 ) as jsonobj
 FROM gene
 LEFT JOIN phylum family_phylum ON gene.family_phylum_id = family_phylum.id
