273108d8fc999c9e0caee6a1d598c38a5aa49a94
Sergey Pushkin <noc@acs.com>
2021-10-15 14:07:56 +0700

    demo & test builds & actions

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
+#!/bin/sh
+TAG=$1
+IMAGE=$TAG
+[ "$IMAGE" = "" ] || IMAGE=":$IMAGE"
+IMAGE="opengenes/api$IMAGE"
 
 BUILD=`git log HEAD --pretty=oneline --relative .| wc -l`
 REVISION=`git rev-parse --short HEAD`
 DATE=`git rev-list --date=short --pretty=%cd HEAD |sed -n 2p`
 echo "build=\"$BUILD\"\nrevision=\"$REVISION\"\ndate=\"$DATE\"" >VERSION
 echo VERSION file written with build $BUILD revision $REVISION date $DATE
+echo image $IMAGE
 
 [ -f .env ] || cat /dev/null >.env
-docker build -t opengenes/api .
+docker build -t $IMAGE .
