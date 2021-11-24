edee70eaab4acc9bbc8a7efe8c492cee3342d4e1
imhelle <sp.olga.inf@gmail.com>
2021-11-24 12:14:09 +0300

    NO_ISSUE disable disease endpoint

--- api/endpoints/disease.py
+++ api/endpoints/disease.py
@@ -13,7 +13,7 @@ router = APIRouter()
     '/disease',
     response_model=List[Disease],
 )
-async def get_disease_list(lang: Language):
+async def get_disease_list(lang: Language = Language.en):
     raise HTTPException(status_code=404, detail='Not implemented', )
     return DiseaseDAO().get()
 
@@ -22,6 +22,6 @@ async def get_disease_list(lang: Language):
     '/disease-category',
     response_model=List[DiseaseCategories],
 )
-async def get_disease_category_list(lang: Language):
+async def get_disease_category_list(lang: Language = Language.en):
     raise HTTPException(status_code=404, detail='Not implemented', )
     return DiseaseDAO().get()
