9dde9a9bf355b32e2a3aad2bae7f6befa6a94dff
sa.zheldokas <sa.zheldokas@gmail.com>
2022-03-29 10:31:37 +0300

    fix gene/{gene}

--- api/endpoints/gene.py
+++ api/endpoints/gene.py
@@ -32,19 +32,6 @@ async def gene_search(input:GeneSearchInput=Depends(GeneSearchInput))->List:
     return GeneDAO().search(input)
 
 @router.get(
-    '/gene/{id_or_symbol}',
-    response_model=GeneSingle
-)
-async def gene_search(id_or_symbol:int|str,input:GeneSingleInput=Depends(GeneSingleInput))->GeneSingle:
-    raise HTTPException( status_code=404, detail='Not yet',)
-    if isinstance(id_or_symbol,int): input.byGeneId=id_or_symbol;
-    if isinstance(id_or_symbol,str): input.bySymbol=id_or_symbol;
-    re=GeneDAO().single(input)
-    if not re:
-        raise HTTPException( status_code=404, detail='Gene not found',)
-    return re
-
-@router.get(
     '/gene/suggestions',
     response_model=GeneSuggestionOutput,
 )
@@ -146,6 +133,20 @@ async def get_gene_by_id(ncbi_id: int, lang: Language = Language.en):
 )
 async def get_gene_by_id(ncbi_id: int, lang: Language = Language.en):
     return 'dummy'
+
+@router.get(
+    '/gene/{id_or_symbol}',
+    response_model=GeneSingle
+)
+async def gene_search(id_or_symbol:int|str,input:GeneSingleInput=Depends(GeneSingleInput))->GeneSingle:
+    raise HTTPException( status_code=404, detail='Not yet',)
+    if isinstance(id_or_symbol,int): input.byGeneId=id_or_symbol;
+    if isinstance(id_or_symbol,str): input.bySymbol=id_or_symbol;
+    re=GeneDAO().single(input)
+    if not re:
+        raise HTTPException( status_code=404, detail='Gene not found',)
+    return re
+
 
 from db.dao import ResearchesDAO
 from models.researches import IncreaseLifespanSearchInput, IncreaseLifespanSearchOutput
