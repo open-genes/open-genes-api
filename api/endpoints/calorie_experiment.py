4eeefc4e33462eff3c38a8a14004b3ea6da668d1
psnewbee <savlovp@mail.ru>
2022-10-03 14:25:17 +0300

    Added byGene, byGeneSymbols, bySuggestions filters

--- api/endpoints/calorie_experiment.py
+++ api/endpoints/calorie_experiment.py
@@ -1,4 +1,5 @@
 from typing import List
+from api.db.dao import GeneSuggestionDAO
 
 from db.dao import CalorieExperimentDAO
 from fastapi import APIRouter, Depends
@@ -11,4 +12,13 @@ router = APIRouter()
 async def increase_lifespan_search(
     input: CalorieExperimentInput = Depends(CalorieExperimentInput),
 ) -> List:
+
+    if input.bySuggestions is not None:
+        sls = GeneSuggestionDAO().search(input.bySuggestions, suggestHidden=0)
+        idls = []
+        for item in sls['items']:
+            idls.append(item['id'])
+        suggfilter = ','.join(str(f) for f in idls) if idls else input.bySuggestions
+        input.bySuggestions = None
+        input.byGeneId = suggfilter
     return CalorieExperimentDAO().calorie_experiment_search(input)
